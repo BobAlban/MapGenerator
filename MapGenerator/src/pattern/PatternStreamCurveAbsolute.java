d6bde8ebf20da8854cd6621928d18045465871b2
BobAlban <alban-marie@sfr.fr>
2018-01-23 18:14:55 +0100

    to end

--- MapGenerator/src/pattern/PatternStreamCurveAbsolute.java
+++ MapGenerator/src/pattern/PatternStreamCurveAbsolute.java
@@ -1,21 +1,48 @@
 package pattern;
 
+import java.util.List;
+
 import hitObject.Pair;
 
-public class PatternStreamCurveAbsolute extends PatternStreamCurve{
+public class PatternStreamCurveAbsolute extends Pattern{
 	/*
 	public PatternStreamCurveAbsolute(int nbObjects, Pair<Double, Double> posFirstObject, double angleStart, double angleCurve, double distanceObject) {
 		super(nbObjects, posFirstObject, angleStart, angleCurve, distanceObject);
 		// TODO Auto-generated constructor stub
 	}*/
 	
-	double angleStart;
+	Pair<Double, Double> posFinalObject;
+	double angleCurve;
+	
 	
 	//angle curve is different of superclass's angle curve
-	public PatternStreamCurveAbsolute(int nbObjects, Pair<Double, Double> posFirstObject, Pair<Double, Double> posFinalObject, double streamCurve) {
-		angleStart = Math.atan((posFinalObject.getRight()-posFirstObject.getRight())/(posFinalObject.getLeft()-posFirstObject.getLeft()));
-		double nbObjectWholeCircle = nbObjects * (2*Math.PI/(streamCurve%(2*Math.PI)));
+	public PatternStreamCurveAbsolute(int nbObjects, Pair<Double, Double> posFirstObject, Pair<Double, Double> posFinalObject, double angleCurve) {
+		super(nbObjects,posFirstObject);
+		this.posFinalObject=posFinalObject;
+		this.angleCurve=angleCurve;
+		Pair<Double,Double> posCenter = getCenter();
+		double angleStart=Math.PI-Math.atan((posCenter.getRight()-posFirstObject.getRight())/(posCenter.getLeft()-posFirstObject.getLeft()));
+		double distanceObject=0.5*getDiameter()*angleCurve/nbObjects;
+		Pattern p = new PatternStreamCurve(nbObjects, posFirstObject, angleStart, angleCurve, distanceObject);
+		listPosition=p.listPosition();
 		
 	}
 	
+	private double getDiameter() {
+		double distanceFirstFinalObject=Math.sqrt(Math.pow(posFinalObject.getLeft()-posFirstObject.getLeft(), 2) + Math.pow(posFinalObject.getRight()-posFirstObject.getRight(), 2));
+		return distanceFirstFinalObject/Math.cos(0.5*(Math.PI-angleCurve));
+	}
+	
+	private Pair<Double,Double> getCenter(){
+		double distanceFirstFinalObject=Math.sqrt(Math.pow(posFinalObject.getLeft()-posFirstObject.getLeft(), 2) + Math.pow(posFinalObject.getRight()-posFirstObject.getRight(), 2));
+		double angleFinalObject=0.5*(Math.PI-angleCurve);
+		double diameter=distanceFirstFinalObject/Math.cos(angleFinalObject);
+		double angleDiameter = angleCurve - angleFinalObject;
+		System.out.println("distanceFirstFinalObject: " + distanceFirstFinalObject + "\ndiameter: " + diameter + "\nangleFinalObject:" + angleFinalObject*180/Math.PI + "\nangleDiameter:" + angleDiameter*180/Math.PI);
+		return new Pair<Double,Double>(posFinalObject.getLeft() + 0.5*diameter*Math.cos(angleDiameter),posFinalObject.getRight() + 0.5*diameter*Math.sin(angleDiameter));
+	}
+
+	public List<Pair<Double, Double>> listPosition() {
+		return listPosition;
+	}	
 }
--- MapGenerator/src/test/TestPattern.java
+++ MapGenerator/src/test/TestPattern.java
@@ -7,12 +7,15 @@ public class TestPattern {
 
 	public static void main(String[] args) throws Exception {
 		Pair<Double,Double> pos = new Pair<Double,Double>(100d,300d);
+		Pair<Double,Double> pos2 = new Pair<Double,Double>(240d,55d);
 		
 		Pattern pattern;
 		/*Square*/
 		//pattern = new PatternJumpPolygon(4, pos, Math.PI/4, 100d, false, 4);
 		/*StreamCurve*/
 		pattern = new PatternStreamCurve(12 , pos, -2*Math.PI/3, 2*Math.PI/3, 30);
+		pattern.showGraph();
+		pattern = new PatternStreamCurveAbsolute(12,pos,pos2,2*Math.PI/3);
 		pattern.showGraph();
 		
 	}
