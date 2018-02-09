b3d57ff98f138f80226cf5ab1fd9bcb3ef113505
BobAlban <alban-marie@sfr.fr>
2018-02-09 20:50:16 +0100

    debugged PatternStreamCurve (initial angle)

--- MapGenerator/src/pattern/PatternStreamCurveAbsolute.java
+++ MapGenerator/src/pattern/PatternStreamCurveAbsolute.java
@@ -10,8 +10,9 @@ public class PatternStreamCurveAbsolute extends PatternStream{
 		super(nbObjects,posFirstObject,angleCurve);
 		this.posFinalObject=posFinalObject;
 		Pair<Double,Double> posCenter = getCenter();
+		System.out.println("PosCenter: " + posCenter);
 		System.out.println("Coef: " + -1/((posCenter.getRight()-posFirstObject.getRight())/(posCenter.getLeft()-posFirstObject.getLeft())));
-		double angleStart=-Math.atan(-1/((posCenter.getRight()-posFirstObject.getRight())/(posCenter.getLeft()-posFirstObject.getLeft())));
+		double angleStart=Math.atan(-1/((posCenter.getRight()-posFirstObject.getRight())/(posCenter.getLeft()-posFirstObject.getLeft())));
 		double distanceObject=getDiameter()*Math.sin(angleCurve/(2*nbObjects));
 		Pattern p = new PatternStreamCurve(nbObjects, posFirstObject, angleCurve, angleStart, distanceObject);
 		System.out.println(toString());
@@ -37,7 +38,7 @@ public class PatternStreamCurveAbsolute extends PatternStream{
 		double radius=0.5*getDiameter();
 		double distanceFirstFinalObject=Math.sqrt(Math.pow(posFinalObject.getLeft()-posFirstObject.getLeft(), 2) + Math.pow(posFinalObject.getRight()-posFirstObject.getRight(), 2));
 		double curve=Math.sqrt(Math.pow(radius/(distanceFirstFinalObject), 2)-0.25);
-		return new Pair<Double,Double>(posMiddle.getLeft()+curve*(posFinalObject.getRight()-posFirstObject.getRight()),posMiddle.getRight()-curve*(posFinalObject.getLeft()-posFirstObject.getLeft()));
+		return new Pair<Double,Double>(posMiddle.getLeft()-(angleCurve/Math.abs(angleCurve))*curve*(posFinalObject.getRight()-posFirstObject.getRight()),posMiddle.getRight()+(angleCurve/Math.abs(angleCurve))*curve*(posFinalObject.getLeft()-posFirstObject.getLeft()));
 	}
 	
 	public String toString() {
--- MapGenerator/src/test/TestPattern.java
+++ MapGenerator/src/test/TestPattern.java
@@ -10,11 +10,11 @@ public class TestPattern {
 		Pair<Double,Double> pos2 = new Pair<Double,Double>(300d,300d);
 		Pattern pattern;
 		
-		//_______________________________________________________________________________________________//
-		//		Square
+		//______Square___________________________________________________________________________________//
+		
 		//pattern = new PatternJumpPolygon(4, pos, Math.PI/4, 100d, false, 4);
-		//_______________________________________________________________________________________________//
-		//		StreamCurve
+		
+		//______StreamCurve______________________________________________________________________________//
 		
 		int nbObjects=12;
 		double angleCurve=(0.5+Math.random())*Math.PI/2;
@@ -27,8 +27,8 @@ public class TestPattern {
 		pattern.showGraph("Immitation");
 		System.out.println(pattern.getListPosition().get(nbObjects-1));
 		
-		//_______________________________________________________________________________________________//
-		//		StreamDoubleAbsolute
+		//______StreamDoubleAbsolute______________________________________________________________________//
+		
 		/*
 		pattern = new PatternStreamDoubleAbsolute(20, pos, 2*Math.PI/6, pos2, true);
 		pattern.showGraph("Graph");
