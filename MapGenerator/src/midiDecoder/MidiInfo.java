fe9d837f511926f6aa6c73606ea74cd3eb5b0327
BobAlban <alban-marie@sfr.fr>
2018-03-22 20:46:51 +0100

    MidiInfo v2 implemented

--- MapGenerator/src/midiDecoder/MidiInfo.java
+++ MapGenerator/src/midiDecoder/MidiInfo.java
@@ -1,35 +1,35 @@
 package midiDecoder;
 
-import javax.sound.midi.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
 
 public class MidiInfo {
-	
-	protected final int NOTE_ON = 0x90;
-	protected final int NOTE_OFF = 0x80;
-	protected String pathMidi;
-	protected ArrayList<Note> sortedMidiEvents;
-	protected float durationTick;
-	protected int resolution;
-    
-    
-    public MidiInfo (String pathMidi) throws Exception {
-    	this.pathMidi = pathMidi;
-    	sortedMidiEvents=sortMidiEvents();
-    	durationTick=durationTick();
-    	//System.out.println(sortedMidiEvents);
-    	resolution=resolution();
-    }
-    
-	public String getpath() {
-		return pathMidi;
+
+	private final int NOTE_ON = 0x90;
+	private final int NOTE_OFF = 0x80;
+	private String pathMidi;
+	private MidiStruct midiStruct;
+	private float durationTick;
+	private int resolution;
+
+	public MidiInfo (String pathMidi) throws Exception {
+		this.pathMidi = pathMidi;
+		midiStruct = new MidiStruct();
+		getMidiEvents();
+		resolution=resolution();
+		durationTick=durationTick();
+		System.out.println(midiStruct);
+		//System.out.println(midiStruct.toStringMilliseconds(durationTick));
 	}
+
+	public String getpath() { return pathMidi; }
 	
-	public ArrayList<Note> getSortedMidiEvents(){
-		return sortedMidiEvents;
-	}
+	public MidiStruct getMidiSturct() {	return midiStruct; }
 	
 	public float getDurationTick() {
 		return durationTick;
@@ -50,53 +50,30 @@ public class MidiInfo {
 		return sequence.getResolution();
 	}
 	
-	private ArrayList<Note> removeStackedNotes(ArrayList<Note> l) throws Exception{
-		int highestVelocity=Integer.MAX_VALUE;
-		for(int i=0;i<l.size();i++) {
-			highestVelocity=l.get(i).getVelocity();
-			int currentStartTick=l.get(i).getStartTick(); 
-			while(i+1<l.size() && currentStartTick==l.get(i+1).getStartTick()) {
-				//System.out.println("l(i):   " + l.get(i) + "\n" + "l(i+1):   " + l.get(i+1));
-				if(highestVelocity<l.get(i+1).getVelocity()) {
-					highestVelocity=l.get(i+1).getVelocity();
-					l.remove(l.get(i));
-				}
-				else {//highestVelocity=>l.get(i+1).getVelocity
-					l.remove(l.get(i+1));
-				}
-			}
-			highestVelocity=Integer.MAX_VALUE;
-		}
-		return l;
-	}
-		
-	/**
-	 * @return a sorted ArrayList of all  NOTE_ON MidiEvents stocked in sortedMidiEvents
-	 */
-	private ArrayList<Note> sortMidiEvents() throws Exception{
-		ArrayList<Note> l = new ArrayList<Note>();
-		
+	private void getMidiEvents() throws Exception {
 		Sequence sequence = MidiSystem.getSequence(new File(pathMidi));
 		for (Track track :  sequence.getTracks()) {
-			 for (int i=0; i < track.size()-1; i++) {
-				 MidiEvent event = track.get(i);
-				 MidiEvent nextEvent = track.get(i+1);
-				 MidiMessage message = event.getMessage();
-				 MidiMessage nextMessage = nextEvent.getMessage();
-				 if (message instanceof ShortMessage && nextMessage instanceof ShortMessage) {
-					 ShortMessage sm = (ShortMessage) message;
-					 ShortMessage nextSm = (ShortMessage) nextMessage;
-					 if (sm.getCommand() == NOTE_ON && sm.getData2()!=0 && (nextSm.getCommand() == NOTE_OFF || nextSm.getData2()==0)) {
-						 Long tick = event.getTick();
-						 Long nextTick = nextEvent.getTick();
-						 
-						 l.add(new Note(tick.intValue(),nextTick.intValue()-tick.intValue(),sm.getData1(),sm.getData2()-nextSm.getData2()));
-					 }
-					 
-				 }
-			 }
+			for (int i=0; i < track.size()-1; i++) {
+				MidiEvent event = track.get(i);
+				MidiEvent nextEvent = track.get(i+1);
+				MidiMessage message = event.getMessage();
+				MidiMessage nextMessage = nextEvent.getMessage();
+				if (message instanceof ShortMessage && nextMessage instanceof ShortMessage) {
+					ShortMessage sm = (ShortMessage) message;
+					ShortMessage nextSm = (ShortMessage) nextMessage;
+					if (sm.getCommand() == NOTE_ON && sm.getData2()!=0 && (nextSm.getCommand() == NOTE_OFF || nextSm.getData2()==0)) {
+						Long tick = event.getTick();
+						Long nextTick = nextEvent.getTick();
+						if(nextTick.intValue()-tick.intValue()!=0) {
+							midiStruct.add(tick.intValue(),nextTick.intValue()-tick.intValue(),sm.getData1(),sm.getData2()-nextSm.getData2());
+						}
+					}
+					else {
+						//System.err.println("C'est bizarre!");
+					}
+				}
+			}
 		}
-		Collections.sort(l, (left, right) -> left.getStartTick() - right.getStartTick());
-		return removeStackedNotes(l);
+		midiStruct.sort();
 	}
 }
--- MapGenerator/src/test/Test.java
+++ MapGenerator/src/test/Test.java
@@ -1,29 +1,11 @@
 package test;
 
-import hitObject.Pair;
+import midiDecoder.MidiInfo;
 
 public class Test {
-	
+
 	public static void main(String[] args) throws Exception{
-		//new MidiInfo("D:\\programs files\\eclipse\\Workspace\\MapGenerator\\src\\test\\test.mid");
-		
-		//double r = Math.random()*2*Math.PI;
-		//System.out.println("x = " + 100*Math.cos(r) + "\ny = " + 100*Math.sin(r));
-		//System.out.println(180*Math.atan(100000)/Math.PI + "\n" + 180*Math.atan(-100000)/Math.PI);
-		Pair<Double,Double> posFirstObject = new Pair<Double,Double>(100d,300d);
-		Pair<Double,Double> posFinalObject = new Pair<Double,Double>(240d,55d);
-		double angleCurve = 2*Math.PI/3;
-		System.out.println(getCenter(posFirstObject,posFinalObject,angleCurve));
-		
+		new MidiInfo("C:\\Users\\Alban\\Desktop\\MapGenerator\\GeneratorDirectory\\Polka\\Polka.mid");	
 	}
-	
-	public static Pair<Double,Double> getCenter(Pair<Double, Double> posFirstObject, Pair<Double, Double> posFinalObject, double angleCurve){
-		double distanceFirstFinalObject=Math.sqrt(Math.pow(posFinalObject.getLeft()-posFirstObject.getLeft(), 2) + Math.pow(posFinalObject.getRight()-posFirstObject.getRight(), 2));
-		double angleFinalObject=0.5*(Math.PI-angleCurve);
-		double diameter=distanceFirstFinalObject/Math.cos(angleFinalObject);
-		double angleDiameter = angleCurve - angleFinalObject;
-		System.out.println("distanceFirstFinalObject: " + distanceFirstFinalObject + "\ndiameter: " + diameter + "\nangleFinalObject:" + angleFinalObject*180/Math.PI + "\nangleDiameter:" + angleDiameter*180/Math.PI);
-		return new Pair<Double,Double>(posFinalObject.getLeft() + 0.5*diameter*Math.cos(angleDiameter),posFinalObject.getRight() + 0.5*diameter*Math.sin(angleDiameter));
-	}
-	
+
 }
